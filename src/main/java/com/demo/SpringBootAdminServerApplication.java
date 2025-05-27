065ae03ffed451ad160123ad17423ddda63ea525
shashikanthhrao <shashikanth_rao@yahoo.com>
2025-05-26 23:51:10 -0700

    dos2unix

--- src/main/java/com/demo/SpringBootAdminServerApplication.java
+++ src/main/java/com/demo/SpringBootAdminServerApplication.java
@@ -9,8 +9,8 @@ import de.codecentric.boot.admin.server.config.EnableAdminServer;
 @SpringBootApplication
 public class SpringBootAdminServerApplication {
 
+	
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootAdminServerApplication.class, args);
-	}
-
+		SpringApplication.run(SpringBootAdminServerApplication.class, args);		
+	}	
 }
